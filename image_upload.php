5f77a6fc1be25c19897234fe02954ac152fd5c5b
TeRaccoon <jack.sam.benning@gmail.com>
2024-01-18 23:36:37 +0000

    Feature: Added image uploading API

---
+++ image_upload.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+
+if ($_FILES['image']) {
+    $image_name = $_FILES['image']['name'];
+    $image_tmp_name = $_FILES['image']['tmp_name'];
+    $error = $_FILES['image']['error'];
+    if ($error > 0) {
+        $response = array(
+            "status" => "error",
+            "error" => true,
+            "message" => "Error uploading the file!"
+        );
+    } else {
+        $uploadDir = '../uploads/';
+        $uploadFile = $uploadDir . basename($image_name);
+
+        if (move_uploaded_file($image_tmp_name, $uploadFile)) {
+            $response = array('success' => true, 'message' => 'Image was uploaded successfully!');
+        } else {
+            $response = array('success' => false, 'message' => 'There was an error uploading the file! The file type may not be valid!');
+        }
+    }
+} else {
+    $response = array('success' => false, 'message' => 'Image was not received by server!');
+}
+
+echo json_encode($response);
