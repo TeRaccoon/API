3497f42531e24d9e2ec0f011afe96f4a8c912848
TeRaccoon <jack.sam.benning@gmail.com>
2024-01-18 23:36:50 +0000

    Refactor: refactor

--- manage_data.php
+++ manage_data.php
@@ -60,14 +60,14 @@ if (isset($data['action'])) {
     echo json_encode($response);
     $database->close_connection();
     exit();
-}
-else {
+} else {
     ErrorHandler::set_error("ERROR: Inconclusive call! Please contact administrator!", "other", "E_SQL-MD-001", null);
     echo "Ruh roh";
     exit();
 }
 
-function insert($conn, $database_utility, $data) {
+function insert($conn, $database_utility, $data)
+{
     $table_name = $data['table_name'];
     $field_names = get_field_names($conn, $table_name);
     $submitted_data = construct_submitted_data($database_utility, $field_names, $table_name, $data);
@@ -75,12 +75,13 @@ function insert($conn, $database_utility, $data) {
 
     handle_image($table_name);
 
-    $conn -> query($query);
-    $conn -> commit();
-    
+    $conn->query($query);
+    $conn->commit();
+
     synchronise($conn, $table_name, null, null);
 }
-function append($conn, $database_utility, $user_database, $customer_database, $data) {
+function append($conn, $database_utility, $user_database, $customer_database, $data)
+{
     if ($data['table_name'] == 'users') {
         $data = append_user($user_database, $data['username'], $data);
     }
@@ -95,13 +96,14 @@ function append($conn, $database_utility, $user_database, $customer_database, $d
 
     handle_image($table_name);
 
-    $conn -> query($query);
-    $conn -> commit();
+    $conn->query($query);
+    $conn->commit();
 
     synchronise($conn, $table_name, $data['id'], $query);
 }
 
-function handle_image($table_name) {
+function handle_image($table_name)
+{
     if (isset($_FILES['image_file_name']) && $_FILES['image_file_name']['error'] == 0) {
         $uploadDir = "../uploads/";
         switch ($table_name) {
@@ -111,7 +113,6 @@ function handle_image($table_name) {
 
             default:
                 return;
-
         }
         $uploadFile = $uploadDir . basename($_FILES["image_file_name"]["name"]);
 
@@ -120,12 +121,13 @@ function handle_image($table_name) {
             echo "File is valid, and was successfully uploaded.";
         } else {
             if ($_POST['action'] == 'add')
-            ErrorHandler::set_error("Warning: There was an error uploading the file! The file may not be valid!", "upload", "E_PHP-MD-001", "The file may already exist on the server in which case this can be ignored. If not, make sure the file is of type JPEG / JPG / PNG.");
+                ErrorHandler::set_error("Warning: There was an error uploading the file! The file may not be valid!", "upload", "E_PHP-MD-001", "The file may already exist on the server in which case this can be ignored. If not, make sure the file is of type JPEG / JPG / PNG.");
         }
     }
 }
-function append_user($user_database, $username, $data) {
-    $current_password = $user_database -> get_user_password($username);
+function append_user($user_database, $username, $data)
+{
+    $current_password = $user_database->get_user_password($username);
 
     if ($current_password != $data['password']) {
         $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 10]);
@@ -134,7 +136,8 @@ function append_user($user_database, $username, $data) {
     return $data;
 }
 
-function append_customer($customer_database, $customer_id, $data) {
+function append_customer($customer_database, $customer_id, $data)
+{
     $current_password = $customer_database->get_customer_password($customer_id);
 
     if ($current_password != $data['password']) {
@@ -144,29 +147,31 @@ function append_customer($customer_database, $customer_id, $data) {
     return $data;
 }
 
-function drop($conn, $data) {
+function drop($conn, $data)
+{
     $table_name = $data['table_name'];
     $ids = $data['id'];
-    
+
     if (str_contains($ids, ",")) {
         $ids = rtrim($ids, ',');
         $query = 'DELETE FROM ' . $table_name . ' WHERE ID IN (' . $ids . ')';
         $ids = explode(',', $ids);
-    }
-    else {
+    } else {
         $query = 'DELETE FROM ' . $table_name . ' WHERE ID = ' . $ids;
     }
-    
+
     synchronise($conn, $table_name, $ids, $query);
 }
 
-function check_date($original_date) {
+function check_date($original_date)
+{
     if ($original_date == null) {
         return null;
     }
     return date('Y-m-d', strtotime($original_date));
 }
-function synchronise($conn, $table_name, $id, $query_string) {
+function synchronise($conn, $table_name, $id, $query_string)
+{
     require_once 'database_functions.php';
     require_once 'database_utility.php';
 
@@ -179,7 +184,7 @@ function synchronise($conn, $table_name, $id, $query_string) {
     $action = $_POST['action'];
 
     if ($id == null) {
-        $id = get_row_contents($conn, "SELECT auto_increment from information_schema.tables WHERE table_name = '".$table_name."' AND table_schema = DATABASE()")[0][0] - 1;
+        $id = get_row_contents($conn, "SELECT auto_increment from information_schema.tables WHERE table_name = '" . $table_name . "' AND table_schema = DATABASE()")[0][0] - 1;
     }
 
     $id = is_array($id) ? $id : [$id];
@@ -197,13 +202,13 @@ function synchronise($conn, $table_name, $id, $query_string) {
                 update_invoiced_item($customer_database, $item_database, $invoiced_item_data, $customer_id);
             }
 
-            if (!$conn -> commit()) {
+            if (!$conn->commit()) {
                 ErrorHandler::set_error('ERROR: ' . $action . ' failed, synchronisation aborted! Please contact administrator!', 'other', 'F_SQL-MD-0002', $conn->error);
                 $conn->abort();
             }
             $debt = $invoice_database->get_customer_debt($customer_id);
 
-            $conn->query("UPDATE customers SET outstanding_balance = '".$debt."' WHERE id = '".$customer_id."'");
+            $conn->query("UPDATE customers SET outstanding_balance = '" . $debt . "' WHERE id = '" . $customer_id . "'");
             break;
 
         case "customer_payments":
@@ -214,7 +219,7 @@ function synchronise($conn, $table_name, $id, $query_string) {
             break;
 
         default:
-            if (!$conn -> commit()) {
+            if (!$conn->commit()) {
                 ErrorHandler::set_error('ERROR: ' . $action . ' failed, synchronisation aborted! Please contact administrator!', 'other', 'F_SQL-MD-0003', $conn->error);
                 echo "Ruh roh";
             }
@@ -222,28 +227,29 @@ function synchronise($conn, $table_name, $id, $query_string) {
     }
 
     if ($query_string != null) {
-        $conn -> query($query_string);
+        $conn->query($query_string);
     }
 
-    if (!$conn -> commit()) {
+    if (!$conn->commit()) {
         ErrorHandler::set_error('ERROR: ' . $action . ' failed, synchronisation aborted! Please contact administrator!', 'other', 'F_SQL-MD-0004', $conn->error);
     }
 }
 
-function update_customer_payment($invoice_database, $customer_database, $customer_payments_database, $payment_data, $id) {
+function update_customer_payment($invoice_database, $customer_database, $customer_payments_database, $payment_data, $id)
+{
     $payment_status = $payment_data['status'];
     $invoice_id = $payment_data['invoice_id'];
     $customer_id = $payment_data['customer_id'];
 
     if ($payment_status == 'Processed' || $_POST['status'] == 'Processed') {
         $total_invoice_payments = $customer_payments_database->get_total_invoice_payments($invoice_id);
-        
     }
 }
-function update_invoiced_item($customer_database, $item_database, $invoiced_item_data, $customer_id) {
+function update_invoiced_item($customer_database, $item_database, $invoiced_item_data, $customer_id)
+{
     $discount = $customer_database->get_customer_discount($customer_id);
     $invoice_values = get_invoice_value($item_database, $invoiced_item_data, $discount);
-    
+
     $item_id = $invoiced_item_data["item_id"];
 
     //Ammending item_invoice total to applied invoice
@@ -253,12 +259,13 @@ function update_invoiced_item($customer_database, $item_database, $invoiced_item
     $total_sold = $item_database->get_calculated_total_sold($item_id);
     $item_database->set_total_sold($total_sold, $item_id);
 }
-function get_invoice_value($item_database, $item_data, $discount) {
+function get_invoice_value($item_database, $item_data, $discount)
+{
     $invoice_id = $item_data["invoice_id"];
     $vat_charge = $item_data["vat_charge"];
 
     $invoiced_item_total = $item_database->get_invoice_total($invoice_id);
-    
+
 
     $net = $discount == 0 ? $invoiced_item_total : $invoiced_item_total * (1 - $discount / 100);
     $vat = $vat_charge == "Yes" ? $net * 0.2 : 0;
@@ -270,27 +277,29 @@ function get_invoice_value($item_database, $item_data, $discount) {
         2 => round($total, 2),
     ];
 }
-function get_row_contents($conn, $query_string) {
+function get_row_contents($conn, $query_string)
+{
     $query = $conn->query($query_string);
     $contents = $query->fetch_all();
     return $contents;
 }
 
-function login($user_database, $data) {
+function login($user_database, $data)
+{
     $username = $data['username'];
     $password = $data['password'];
 
     $password_hash = $user_database->get_user_password($username);
     if ($password_hash != null && password_verify($password, $password_hash)) {
         $response = array('success' => true, 'message' => 'Login successful');
-    }
-    else {
+    } else {
         $response = array('success' => false, 'message' => 'Invalid credentials');
     }
     echo json_encode($response);
     exit();
-  }
-  function create_account($user_database) {
+}
+function create_account($user_database)
+{
     require 'dbh.php';
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT, ['cost' => 10]);
@@ -299,27 +308,28 @@ function login($user_database, $data) {
     if ($rows != 0) {
         $_SESSION['mysql_error'] =  "Error: A user with that username is taken!";
         header("Location: {$_SERVER["HTTP_REFERER"]}");
-    } 
-    else {
+    } else {
         $stmt = $conn->prepare("INSERT INTO users (`username`, `password`, `level`) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $username, $password, $access_level);
         $stmt->execute();
         header("Location: {$_SERVER["HTTP_REFERER"]}");
         exit();
     }
-  }
+}
 
-  function get_field_names($conn, $table_name) {
-    $query = $conn->query('SHOW FULL COLUMNS FROM '. $table_name);
+function get_field_names($conn, $table_name)
+{
+    $query = $conn->query('SHOW FULL COLUMNS FROM ' . $table_name);
     while ($row = $query->fetch_assoc()) {
         if ($row['Extra'] == null) {
             $field_names[] = $row['Field'];
         }
     }
     return $field_names;
-  }
+}
 
-function construct_submitted_data($db_utility, $field_names, $table_name, $data) {
+function construct_submitted_data($db_utility, $field_names, $table_name, $data)
+{
     $submitted_data = [];
     foreach ($field_names as $field_name) {
         $type = $db_utility->get_type_from_field($table_name, $field_name);
@@ -333,8 +343,7 @@ function construct_submitted_data($db_utility, $field_names, $table_name, $data)
                 if ($_FILES[$field_name]['name'] == null) {
                     $file_name = $db_utility->recover_retail_image($data['id']);
                     $data[$field_name] = $file_name;
-                }
-                else {
+                } else {
                     $data[$field_name] = $_FILES[$field_name]['name'];
                 }
             }
@@ -343,4 +352,3 @@ function construct_submitted_data($db_utility, $field_names, $table_name, $data)
     }
     return $submitted_data;
 }
-?>
